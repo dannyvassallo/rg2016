1e0d5ea92906f922585a89a8e0c6e31afd7ac63a
dannyvassallo <danielvassallo87@gmail.com>
2016-03-29 19:10:53 -0400

    created scaffolding for classes

---
+++ app/controllers/rutgers_classes_controller.rb
@@ -0,0 +1,23 @@
+class RutgersClassesController < ApplicationController
+  def index
+    @classes = RutgersClass.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
