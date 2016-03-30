c413d779bfd0e1e1f79a2944a94a91c01325393b
dannyvassallo <danielvassallo87@gmail.com>
2016-03-29 20:06:57 -0400

    created all resources for classes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources 'rutgers_classes'
+  resources :rutgers_classes, :path => "classes"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
